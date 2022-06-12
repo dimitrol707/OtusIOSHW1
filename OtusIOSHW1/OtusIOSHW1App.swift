a3fb2f6368364a4ae5f14738ceaa6c33fda308ea
DIM <dimitrol7171@gmail.com>
2022-06-12 18:26:34 +0300

    Initial Commit

---
+++ OtusIOSHW1/OtusIOSHW1App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusIOSHW1App.swift
+//  OtusIOSHW1
+//
+//  Created by Дмитрий Мальцев on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct OtusIOSHW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
